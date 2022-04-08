e62bc2f82affbbe051c255fbcd1b5481a17dbbce
ngocminh20it920 <102497496+ngocminh20it920@users.noreply.github.com>
2022-04-08 08:33:10 +0700

    Add files via upload

---
+++ Lab5_NguyenNgocMinh20IT920/app/src/main/java/com/example/lab05/ClickerApplication.kt
@@ -0,0 +1,12 @@
+package com.example.lab05
+
+import android.app.Application
+import timber.log.Timber
+
+class ClickerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
